498d6c2fc8a5dbf57faab83b55b7e407d8c297a1
Oscar Michel <oscarmichelh@gmail.com>
2019-09-27 01:31:34 -0500

    remove rpsuh model

--- db/schema.rb
+++ db/schema.rb
@@ -86,73 +86,73 @@ ActiveRecord::Schema.define(version: 2019_09_21_214345) do
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
-  create_table "rpush_apps", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "environment"
-    t.text "certificate"
-    t.string "password"
-    t.integer "connections", default: 1, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "type", null: false
-    t.string "auth_key"
-    t.string "client_id"
-    t.string "client_secret"
-    t.string "access_token"
-    t.datetime "access_token_expiration", precision: 6
-    t.text "apn_key"
-    t.string "apn_key_id"
-    t.string "team_id"
-    t.string "bundle_id"
-    t.boolean "feedback_enabled", default: true
-  end
-
-  create_table "rpush_feedback", force: :cascade do |t|
-    t.string "device_token"
-    t.datetime "failed_at", precision: 6, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "app_id"
-    t.index ["device_token"], name: "index_rpush_feedback_on_device_token"
-  end
-
-  create_table "rpush_notifications", force: :cascade do |t|
-    t.integer "badge"
-    t.string "device_token"
-    t.string "sound"
-    t.text "alert"
-    t.text "data"
-    t.integer "expiry", default: 86400
-    t.boolean "delivered", default: false, null: false
-    t.datetime "delivered_at", precision: 6
-    t.boolean "failed", default: false, null: false
-    t.datetime "failed_at", precision: 6
-    t.integer "error_code"
-    t.text "error_description"
-    t.datetime "deliver_after", precision: 6
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "alert_is_json", default: false, null: false
-    t.string "type", null: false
-    t.string "collapse_key"
-    t.boolean "delay_while_idle", default: false, null: false
-    t.text "registration_ids"
-    t.integer "app_id", null: false
-    t.integer "retries", default: 0
-    t.string "uri"
-    t.datetime "fail_after", precision: 6
-    t.boolean "processing", default: false, null: false
-    t.integer "priority"
-    t.text "url_args"
-    t.string "category"
-    t.boolean "content_available", default: false, null: false
-    t.text "notification"
-    t.boolean "mutable_content", default: false, null: false
-    t.string "external_device_id"
-    t.string "thread_id"
-    t.boolean "dry_run", default: false, null: false
-    t.index ["delivered", "failed", "processing", "deliver_after", "created_at"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))"
-  end
+  # create_table "rpush_apps", force: :cascade do |t|
+  #   t.string "name", null: false
+  #   t.string "environment"
+  #   t.text "certificate"
+  #   t.string "password"
+  #   t.integer "connections", default: 1, null: false
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  #   t.string "type", null: false
+  #   t.string "auth_key"
+  #   t.string "client_id"
+  #   t.string "client_secret"
+  #   t.string "access_token"
+  #   t.datetime "access_token_expiration", precision: 6
+  #   t.text "apn_key"
+  #   t.string "apn_key_id"
+  #   t.string "team_id"
+  #   t.string "bundle_id"
+  #   t.boolean "feedback_enabled", default: true
+  # end
+  #
+  # create_table "rpush_feedback", force: :cascade do |t|
+  #   t.string "device_token"
+  #   t.datetime "failed_at", precision: 6, null: false
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  #   t.integer "app_id"
+  #   t.index ["device_token"], name: "index_rpush_feedback_on_device_token"
+  # end
+  #
+  # create_table "rpush_notifications", force: :cascade do |t|
+  #   t.integer "badge"
+  #   t.string "device_token"
+  #   t.string "sound"
+  #   t.text "alert"
+  #   t.text "data"
+  #   t.integer "expiry", default: 86400
+  #   t.boolean "delivered", default: false, null: false
+  #   t.datetime "delivered_at", precision: 6
+  #   t.boolean "failed", default: false, null: false
+  #   t.datetime "failed_at", precision: 6
+  #   t.integer "error_code"
+  #   t.text "error_description"
+  #   t.datetime "deliver_after", precision: 6
+  #   t.datetime "created_at", precision: 6, null: false
+  #   t.datetime "updated_at", precision: 6, null: false
+  #   t.boolean "alert_is_json", default: false, null: false
+  #   t.string "type", null: false
+  #   t.string "collapse_key"
+  #   t.boolean "delay_while_idle", default: false, null: false
+  #   t.text "registration_ids"
+  #   t.integer "app_id", null: false
+  #   t.integer "retries", default: 0
+  #   t.string "uri"
+  #   t.datetime "fail_after", precision: 6
+  #   t.boolean "processing", default: false, null: false
+  #   t.integer "priority"
+  #   t.text "url_args"
+  #   t.string "category"
+  #   t.boolean "content_available", default: false, null: false
+  #   t.text "notification"
+  #   t.boolean "mutable_content", default: false, null: false
+  #   t.string "external_device_id"
+  #   t.string "thread_id"
+  #   t.boolean "dry_run", default: false, null: false
+  #   t.index ["delivered", "failed", "processing", "deliver_after", "created_at"], name: "index_rpush_notifications_multi", where: "((NOT delivered) AND (NOT failed))"
+  # end
 
   add_foreign_key "devices", "users"
   add_foreign_key "notifications", "users"
