b8e503b1352aa18d18355c58903ec9ad1747e71b
Oscar Michel <oscarmichelh@gmail.com>
2019-11-11 22:53:23 -0600

    Changed timezone to Monterrey

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module OxfordAppApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Monterrey'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -43,6 +45,5 @@ module OxfordAppApi
         )
       end
     end
-
   end
 end
